a8e1356f5b116fd1d4fc771f56f7c53fefb86391
Guillermo Aguirre <guillermoaguirre1@gmail.com>
2019-07-04 14:20:46 -0300

    Add log to arrayresponse

--- scrapy/http/response/array.py
+++ scrapy/http/response/array.py
@@ -4,10 +4,13 @@ discovering (through HTTP headers) to base Response class.
 
 See documentation in docs/topics/request-response.rst
 """
+import logging
 
 from scrapy.http.response import Response
+logger = logging.getLogger(__name__)
 
 class ArrayResponse(Response):
 
     def _set_body(self, body):
+        logger.info('Body length: %s', len(body))
         self._body = body
